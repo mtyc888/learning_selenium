edab83bfa29d0fc24f31079ce9923a07ad5e8128
marvin <ymarvintan@gmail.com>
2026-03-18 00:19:18 +0800

    update

--- test.py
+++ test.py
@@ -32,7 +32,27 @@ def scrape():
     driver = get_driver(headless=False)
 
     driver.get('https://seths.blog/')
+
+    # scoping, because our target text is within this element
     post_container = driver.find_element(By.CLASS_NAME, 'post')
+    """
+        :nth-of-type()
+
+        if the element within a div has no class, you can use this.
+        eg:
+
+        <div class="post">
+            <p class="wp-block-paragraph">
+                ABC
+            </p>
+            <p class="wp-block-paragraph">
+                DEF
+            </p>
+        </div>
+
+        p.wp-block-paragraph:nth-of-type(1) => ABC
+        p.wp-block-paragraph:nth-of-type(2) => DEF
+    """
     # We were taught to look out for red flags. Little signs that something is wrong, that we should be careful or even turn around.
     text_element = post_container.find_element(By.CSS_SELECTOR, 'p.wp-block-paragraph:nth-of-type(1)')
     print(text_element.text)
